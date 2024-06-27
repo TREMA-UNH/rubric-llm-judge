5714051cc3e665091f3eb8fbf72b6b101cb00df0
Laura <dietz@cs.unh.edu>
2024-06-27 12:54:16 -0400

    flake: initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "rubric-llm-judge";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.dspy-nix.url = "git+https://git.smart-cactus.org/ben/dspy-nix";
+  inputs.exampp.url = "git+ssh://git@git.smart-cactus.org/ben/exampp.git";
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, dspy-nix, exampp, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        mkShell = target: (dspy-nix.lib.${system}.mkShell {
+          inherit target;
+          pythonOverrides = [ exampp.lib.${system}.pythonOverrides ];
+          packages = ps: [ ps.exampp ];
+        });
+
+      in {
+        packages.exampp = (pkgs.python3.override {packageOverrides = exampp.lib.${system}.pythonOverrides;}).pkgs.exampp;
+        devShells.default = self.outputs.devShells.${system}.cuda;
+        devShells.cpu = mkShell "cpu";
+        devShells.rocm = mkShell "rocm";
+        devShells.cuda = mkShell "cuda";
+      }
+    );
+}
